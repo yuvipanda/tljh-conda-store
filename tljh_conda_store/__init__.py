0ea1a4ad6018447f995deb02b571f740dffaee40
YuviPanda <yuvipanda@gmail.com>
2021-11-02 03:43:14 +0530

    Create separate conda env for conda-server
    Needs conda as a dependency, which can't be installed in
    a virtual environment

--- tljh_conda_store/__init__.py
+++ tljh_conda_store/__init__.py
@@ -3,28 +3,17 @@ Simplest plugin that exercises all the hooks
 """
 from tljh.hooks import hookimpl
 from pathlib import Path
+import subprocess
+import os
 import sys
 
 @hookimpl
 def tljh_extra_apt_packages():
     return [
-        'postgresql',
+        'libpq-dev',
     ]
 
 
-@hookimpl
-def tljh_config_post_install(config):
-    # Put an arbitrary marker we can test for
-    config['simplest_plugin'] = {
-        'present': True
-    }
-
-@hookimpl
-def tljh_extra_hub_pip_packages():
-    return [
-        'psycopg2-binary',
-        '/srv/src/conda-store/conda-store-server'
-    ]
 
 @hookimpl
 def tljh_custom_jupyterhub_config(c):
@@ -35,8 +24,21 @@ def tljh_custom_jupyterhub_config(c):
             'admin': True,
             'url': 'http://localhost:5000',
             'command': [
-                '/opt/tljh/hub/bin/conda-store-server',
+                '/opt/conda-store/server/bin/conda-store-server',
                 '--config', str(conda_store_config)
-            ]
+            ],
+            'oauth_redirect_uri': 'http://localhost:12000/services/conda-store/oauth_callback/'
         }
     ]
+
+
+@hookimpl
+def tljh_post_install():
+    conda_store_base = Path('/opt/conda-store')
+    for d in ['store', 'envs']:
+        os.makedirs(conda_store_base / d, exist_ok=True)
+    subprocess.check_call([
+        '/opt/tljh/user/bin/mamba', 'create', '--prefix', str(conda_store_base / 'server'),
+        '-c', 'conda-forge',
+        'conda-store-server', '--json', '--yes'
+    ])
